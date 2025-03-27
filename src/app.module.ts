49f21c6c66055815e5f3c6cc611b87d31ae2d37e
sunwei3104 <sunmai3104@gmail.com>
2025-03-27 16:23:28 +0700

    ลบคอมเม้นท์

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module'; // นำเข้า UsersModule
-import { AuthModule } from './auth/auth.module'; // นำเข้า AuthModule
+import { UsersModule } from './users/users.module'; 
+import { AuthModule } from './auth/auth.module'; 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql', // ชนิดของฐานข้อมูล
-      host: 'localhost', // เซิร์ฟเวอร์ที่ใช้
-      port: 3306, // พอร์ตที่ใช้ (MySQL พอร์ต 3306)
-      username: 'root', // ชื่อผู้ใช้ฐานข้อมูล
-      password: 'your-password', // รหัสผ่าน
-      database: 'your-database', // ชื่อฐานข้อมูล
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], // เส้นทางของ entity ที่จะใช้ในโปรเจค
-      synchronize: true, // ควบคุมการสร้าง schema
+      type: 'mysql', 
+      host: 'localhost', 
+      port: 3306, 
+      username: 'root', 
+      password: 'your-password', 
+      database: 'your-database', 
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], 
+      synchronize: true, 
     }),
-    UsersModule, // นำเข้า UsersModule เพื่อให้สามารถใช้ repository และ entity
-    AuthModule, // นำเข้า AuthModule ถ้ามีการใช้
+    UsersModule, 
+    AuthModule, 
   ],
 })
-export class AppModule {}
+export class AppModule {}  
